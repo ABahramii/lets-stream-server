22b6dc8708b40ab01f758057e37cfce17ee11a4e
amir <amirbahraami99@gmail.com>
2022-11-10 12:57:58 +0330

    init commit

---
+++ src/main/java/ir/stream/LetsStreamServerApplication.java
@@ -0,0 +1,13 @@
+package ir.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsStreamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsStreamServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/stream/LetsStreamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsStreamServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
