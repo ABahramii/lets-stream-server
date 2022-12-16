6fe2301b6ba6e3ec67b7c28c05efa1f080ab6510
amir <amirbahraami99@gmail.com>
2022-12-16 15:28:04 +0330

    change CacheUtils

--- src/main/java/ir/stream/utils/CacheUtils.java
+++ src/main/java/ir/stream/utils/CacheUtils.java
@@ -7,14 +7,13 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.random.RandomGenerator;
 
 public class CacheUtils {
     public static final Map<Long, Set<User>> roomMemberMap = new HashMap<>();
     public static final Map<Long, Set<Message>> roomMessageMap = new HashMap<>();
 
     static {
-        User user = new User(RandomGenerator.getDefault().nextLong(), "amir");
+//        User user = new User(RandomGenerator.getDefault().nextLong(), "amir");
 //        Room room = new Room(RandomGenerator.getDefault().nextLong(), "amir", user);
         roomMemberMap.put(1L, new HashSet<>());
         roomMessageMap.put(1L, new HashSet<>());
