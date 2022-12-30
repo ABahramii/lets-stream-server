dfd4297ad4198b7d6276514187725489e65f7c1b
amir <amirbahraami99@gmail.com>
2022-12-30 12:24:43 +0330

    add test data to runner

--- src/main/java/ir/stream/LetsStreamServerApplication.java
+++ src/main/java/ir/stream/LetsStreamServerApplication.java
@@ -1,10 +1,7 @@
 package ir.stream;
 
-import ir.stream.app.entity.Role;
-import ir.stream.app.entity.User;
-import ir.stream.app.service.RoleService;
-import ir.stream.app.service.UserRoleService;
-import ir.stream.app.service.UserService;
+import ir.stream.app.entity.*;
+import ir.stream.app.service.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,7 +19,9 @@ public class LetsStreamServerApplication {
     }
 
     @Bean
-    public CommandLineRunner run(UserService userService, RoleService roleService, UserRoleService userRoleService) {
+    public CommandLineRunner run(UserService userService, RoleService roleService, UserRoleService userRoleService,
+                                 RoomService roomService, GuestService guestService, RoomUserService roomUserService,
+                                 ChatService chatService) {
         PasswordEncoder passwordEncoder = passwordEncoder();
 
         return args -> {
@@ -46,6 +45,33 @@ public class LetsStreamServerApplication {
             userRoleService.create(user1, roleAdmin);
             userRoleService.create(user2, roleManager);
             userRoleService.create(user3, roleUser);
+
+            Room room = new Room("name", user1, new HashSet<>(), new HashSet<>());
+            roomService.save(room);
+
+            Guest guest1 = new Guest("guest-1", room);
+            Guest guest2 = new Guest("guest-2", room);
+            guestService.save(guest1);
+            guestService.save(guest2);
+
+            RoomUser roomUser1 = new RoomUser(room, user1);
+            RoomUser roomUser2 = new RoomUser(room, user2);
+            RoomUser roomUser3 = new RoomUser(room, user3);
+            roomUserService.save(roomUser1);
+            roomUserService.save(roomUser2);
+            roomUserService.save(roomUser3);
+
+            Chat chat1 = new Chat("hello world, java is the king,,,", "12 AM", user2.getUsername(), true, room);
+            Chat chat2 = new Chat("js is also good.", "12:13 AM", guest1.getName(), false, room);
+            Chat chat3 = new Chat("but rust will overcome :)", "12:16 AM", user3.getUsername(), true, room);
+            Chat chat4 = new Chat("ha ha ha", "12:16 AM", user1.getUsername(), true, room);
+            Chat chat5 = new Chat("talk is cheap, show me the code", "12:30 AM", guest2.getName(), false, room);
+            chatService.save(chat1);
+            chatService.save(chat2);
+            chatService.save(chat3);
+            chatService.save(chat4);
+            chatService.save(chat5);
+
         };
     }
 
