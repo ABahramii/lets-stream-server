5ef4907427fe878815bd3549c02a6b21057b8173
amir <amirbahraami99@gmail.com>
2023-01-05 12:16:01 +0330

    UsernameTokenDTO added

---
+++ src/main/java/ir/stream/app/dto/UsernameTokenDTO.java
@@ -0,0 +1,15 @@
+package ir.stream.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsernameTokenDTO {
+    private String username;
+    private String token;
+}
